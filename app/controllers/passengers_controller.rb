8946fe97f9e0e677d8331ce79c1181ddb1484aed
Anibel America <ameriabel@gmail.com>
2018-10-02 16:16:46 -0700

    adds create action

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -14,5 +14,25 @@ class PassengersController < ApplicationController
     @passenger = Passenger.new
   end
 
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passengers_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(
+      :name,
+      :phone_num
+    )
+  end
 
 end
