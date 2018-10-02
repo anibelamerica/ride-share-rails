a9c85b460f545e91e8dbb8520e15ef5406d96401
Anibel America <ameriabel@gmail.com>
2018-10-02 16:54:18 -0700

    adds driver index and view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,19 @@
 class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+
+  
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(
+      :name,
+      :phone_num
+    )
+  end
+
+
 end
