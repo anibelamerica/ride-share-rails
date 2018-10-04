c7fb8aa73036bbc375e4e41c905f497b03189ec1
Anibel America <ameriabel@gmail.com>
2018-10-04 15:31:02 -0700

    fixes average rating for missing data

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -9,11 +9,13 @@ class Driver < ApplicationRecord
   end
 
   def average_rating
-    return (self.trips.sum { |trip| trip.rating} * 1.0 / self.trips.length).round(1)
+    rated_trips = self.trips.select { |trip| trip.rating }
+
+    return (rated_trips.sum { |trip| trip.rating} * 1.0 / self.trips.length).round(1)
   end
 
   def self.search(search)
-  where("name ILIKE ?", "%#{search}%") 
+    where("name ILIKE ?", "%#{search}%")
   end
 
 end
