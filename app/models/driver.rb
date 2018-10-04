4b3bb4f31424cc58e81ba5c4287d0270f89d1de0
Anibel America <ameriabel@gmail.com>
2018-10-04 12:34:43 -0700

    adds method and view for driver's total earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,12 @@
 class Driver < ApplicationRecord
   validates :name, presence: true
   validates :vin, presence: true
+
   has_many :trips
+
+  def total_earnings
+    return self.trips.sum { |trip| (trip.cost - 1.65) * 0.80 / 100}.round(2)
+  end
+
+
 end
