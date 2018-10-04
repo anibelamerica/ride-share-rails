88d5016d041305be881120fc3cfb17ba3e1f1a2b
Anibel America <ameriabel@gmail.com>
2018-10-04 12:43:58 -0700

    adds driver average rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -8,5 +8,9 @@ class Driver < ApplicationRecord
     return self.trips.sum { |trip| (trip.cost - 1.65) * 0.80 / 100}.round(2)
   end
 
+  def average_rating
+    return (self.trips.sum { |trip| trip.rating} * 1.0 / self.trips.length).round(1)
+  end
+
 
 end
