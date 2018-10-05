838939d1c101339d8c3d10cefeb7fc21ebd18b66
Anibel America <ameriabel@gmail.com>
2018-10-05 15:19:05 -0700

    adds cascading delete functionality to driver and passenger delete action

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
   validates :name, presence: true
   validates :phone_num, presence: true
-  has_many :trips
+  has_many :trips, dependent: :delete_all
 
   def amt_charged
     return self.trips.sum { |trip| trip.cost } * 1.0 / 100
